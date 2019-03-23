cdc35897122347bc6d38b6af68fbf523ff5ab6b3
a4a881d4 <a4a881d4@163.com>
2019-03-23 12:57:26 +0800

    extend

--- app.js
+++ app.js
@@ -15,12 +15,16 @@ pa.forEach(function(fn) {
   }
   res[getName(fn)] = result
 })
+var f0 = process.argv[2]
+var nRes = res[f0]
 
-console.log(YAML.stringify(res,8,2))
+extendObj(nRes,res)
 
+console.log(YAML.stringify(nRes,24,2))
+//console.log(YAML.stringify(res,24,2))
 function getName(fileName) {
   var indexF = fileName.lastIndexOf('.')
-  return fileName.substring(0,indexF-1)
+  return fileName.substring(0,indexF)
 }
 
 function nextObj(jobj,fn) {
@@ -40,6 +44,28 @@ function nextObj(jobj,fn) {
       //console.log("A: "+key+':'+jobj[key])
     } else if(v.length > 0 && typeof(v) == "object" || typeof(v) == "object") {
       nextObj(v,fn)
+    }
+  }
+}
+
+function extendObj(jobj,allyaml) {
+  for(var key in jobj) {
+    var v = jobj[key]
+    if(v['$ref'] != null) {
+      var ref = v['$ref']
+      var rs = ref.substring(2,ref.length)
+      var s = rs.split('/')
+      // console.log(s)
+      var robj = allyaml
+      for(var ss in s) {
+        // console.log(s[ss])
+        robj = robj[s[ss]]
+      }
+      // console.log(robj)
+      jobj[key] = robj
+      extendObj(robj,allyaml)
+    } else if(v.length > 0 && typeof(v) == "object" || typeof(v) == "object") {
+      extendObj(v,allyaml)
     }
   }
 }
